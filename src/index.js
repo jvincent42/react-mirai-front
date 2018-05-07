a4dff4eadcbf558dc014524af7792085950bc600
jvincent <jeremy.vincent.external@sigfox.com>
2018-05-07 23:43:31 +0200

    chore(package): screw redux

--- src/index.js
+++ src/index.js
@@ -3,18 +3,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { AppContainer } from 'react-hot-loader';
 import { ThemeProvider } from 'styled-components';
-import { Provider } from 'react-redux';
 
 import theme from './App/theme';
-import store from './App/store';
 
 
 const Application = ({ children }) => (
   <AppContainer>
     <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        {children}
-      </Provider>
+      {children}
     </ThemeProvider>
   </AppContainer>
 );
